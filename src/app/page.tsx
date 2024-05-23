92220b4f99dc26a2acc71d3c0e2a96ef62360230
Felipegithubf <felipecarvalho5055@gmail.com>
2024-05-23 10:03:22 -0300

    Atualizando

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,21 @@
 import { Button, Typography } from "@mui/material";
-import Image from "@/app/imagens/galactic-night-sky-astronomy-science-combined-generative-ai.jpg";
 
 const Login = () => {
   return (
-    <div className="w-screen h-screen p-24 bg-gray-800 flex justify-center items-center">
-      <div className="max-w-md w-full">
+    
+    <div className="w-screen h-screen p-24 flex justify-center items-center opacity-100" style={{ backgroundColor: "#2A8EFF" }}>
+      <header className="w-screen h-screen flex border border-white  ">
+        
+      </header>
+      <div className="max-w-md p-20 w-full border border-white-600 rounded-md  bg-blue-700 bg-opacity-90">
         <Typography className="text-3xl font-bold text-center text-white mb-6">Sistema de Login</Typography>
         <div className="my-6">
           <label className="block text-lg mb-2 text-white" htmlFor="emailField">Endereço de e-mail</label>
-          <input className="w-full p-2 rounded-md text-lg bg-gray-800 border border-gray-600 outline-blue-400 text-white" type="email" id="emailField"/>
+          <input className="w-full p-2 rounded-md text-lg bg-gray-800 bg-opacity-70 border border-gray-600 outline-blue-400 text-white" type="email" id="emailField"/>
         </div>
         <div className="my-6">
           <label className="block text-lg mb-2 text-white" htmlFor="passwordField">Senha</label>
-          <input className="w-full p-2 rounded-md text-lg bg-gray-800 border border-gray-600 outline-blue-400 text-white" type="password" id="passwordField"/>
+          <input className="w-full p-2 rounded-md text-lg bg-gray-800 bg-opacity-70 border border-gray-600 outline-blue-400 text-white" type="password" id="passwordField"/>
         </div>
         <div className="my-6">
           <Button className="w-full p-2 rounded-md text-lg font-bold border-none text-white bg-yellow-500">Entrar</Button> 
