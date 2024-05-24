0e3d2cd8a048ea88ac43210a01b980c621db17d5
Felipegithubf <felipecarvalho5055@gmail.com>
2024-05-24 14:38:57 -0300

    Atualização da borda lateral

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,17 @@ import { Button, Typography } from "@mui/material";
 
 const Login = () => {
   return (
-    
     <div className="w-screen h-screen p-24 flex justify-center items-center opacity-100" style={{ backgroundColor: "#2A8EFF" }}>
-      <header className="w-screen h-screen flex border border-white  ">
-        
-      </header>
-      <div className="max-w-md p-20 w-full border border-white-600 rounded-md  bg-blue-700 bg-opacity-90">
-        <Typography className="text-3xl font-bold text-center text-white mb-6">Sistema de Login</Typography>
+      <header className="w-full h-7 flex justify-center items-center md:mx-12 lg:mx-24 relative">
+ 
+      <div className="max-w-lp  p-80 w-full border-white-600 rounded-md border-4 bg-gray-200 bg-opacity-90">
+
+      </div>
+ 
+  
+</header>
+      <div className="max-w-md p-20 w-full border border-white-600 rounded-md  bg-blue-5800 bg-opacity-90">
+        <Typography className="text-3xl font-bold text-center text-white mb-6">VHA Login</Typography>
         <div className="my-6">
           <label className="block text-lg mb-2 text-white" htmlFor="emailField">Endereço de e-mail</label>
           <input className="w-full p-2 rounded-md text-lg bg-gray-800 bg-opacity-70 border border-gray-600 outline-blue-400 text-white" type="email" id="emailField"/>
@@ -20,7 +24,7 @@ const Login = () => {
         <div className="my-6">
           <Button className="w-full p-2 rounded-md text-lg font-bold border-none text-white bg-yellow-500">Entrar</Button> 
         </div>
-        <div className="text-center text-gray-400">
+        <div className="text-center text-blue-900">
           fez seu cadastro? <a className="text-yellow-500 font-bold" href=""> Faça seu cadastro agora</a>
         </div>
       </div>
