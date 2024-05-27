88852724b5048d9bd23d97abf590272808ea3d14
Felipegithubf <felipecarvalho5055@gmail.com>
2024-05-27 14:56:39 -0300

    Alterações..

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,73 +1,75 @@
-'use client'
+'use client'; // Não está claro o que 'use client' significa aqui, pode ser específico de um framework ou biblioteca.
 
-import { useState } from 'react';
-import { Button, Typography } from "@mui/material";
-import {} from '@/auth/Dashboard';
+import { useState } from 'react'; // Importa o hook useState do React para gerenciar estados em componentes funcionais.
+import { Button, Typography } from "@mui/material"; // Importa os componentes Button e Typography da biblioteca Material-UI.
+import Image from 'next/image'; // Importa o componente Image do Next.js para otimização de imagens.
 
 const Login = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [username, setUsername] = useState(''); // Novo estado para o nome de usuário
-  const [error, setError] = useState('');
-  const [isRegistering, setIsRegistering] = useState(false); // Estado para controlar se está no modo de registro ou não
+  // Estados do componente
+  const [email, setEmail] = useState(''); // Estado para armazenar o valor do campo de email.
+  const [password, setPassword] = useState(''); // Estado para armazenar o valor do campo de senha.
+  const [rememberMe, setRememberMe] = useState(false); // Estado para armazenar se o usuário deseja ser lembrado.
+  const [error, setError] = useState(''); // Estado para armazenar mensagens de erro.
+  const [isRegistering, setIsRegistering] = useState(false); // Estado para controlar se o usuário está no modo de registro.
 
-  const handleSubmit = async (e: any) => {
+  // Função para lidar com o envio do formulário
+  const handleSubmit = async (e:any) => {
     e.preventDefault();
 
     if (isRegistering) {
       // Lógica de cadastro
-      // Aqui você pode enviar os dados para o servidor para cadastro
-      // Por exemplo: 
-      // const response = await fetch('/api/register', {
-      //   method: 'POST',
-      //   body: JSON.stringify({ username, email, password }),
-      //   headers: {
-      //     'Content-Type': 'application/json'
-      //   }
-      // });
-      // Se o cadastro for bem-sucedido, você pode redirecionar o usuário para a página inicial ou mostrar uma mensagem de sucesso
-      // Se houver algum erro durante o cadastro, você pode exibir uma mensagem de erro
     } else {
       // Simulação de autenticação básica
-      if (email === 'usuario@example.com' && password === 'senha123') {
-        // Se as credenciais estiverem corretas, redirecione o usuário para a página inicial (ou faça o que quiser)
-        window.location.href = '/';
-      } else {
-        // Se as credenciais estiverem incorretas, exiba uma mensagem de erro
-        setError('Por favor, tente novamente.');
-      }
     }
   };
 
+  // Renderização do componente
   return (
-    // Uso do cliente para a interface de login
     <div className="w-screen h-screen p-24 flex justify-center items-center opacity-100" style={{ backgroundColor: "#2A8EFF" }}>
+      {/* Cabeçalho */}
       <header className="w-full h-7 flex justify-center items-center md:mx-12 lg:mx-24 relative">
-        <div className="max-w-lp  p-80 w-full border-white-600 rounded-md border-4 bg-gray-200 bg-opacity-90"></div>
+        <div className="max-w-lp  p-80 w-full border-white-600 rounded-md border-4 bg-gray-200 bg-opacity-90">
+          <Image src="/galactic-night-sky-astronomy-science-combined-generative-ai.jpg" alt="Céu noturno galáctico" width={800} height={1500} />
+        </div>
       </header>
-      
+      {/* Formulário de login ou registro */}
       <div className="max-w-md p-20 w-full border border-white-600 rounded-md  bg-blue-5800 bg-opacity-90">
         <Typography className="text-3xl font-bold text-center text-white mb-6">VHA {isRegistering ? 'Cadastro' : 'Login'}</Typography>
         <form onSubmit={handleSubmit}>
+          {/* Campo de nome de usuário, visível apenas durante o registro */}
           {isRegistering && (
             <div className="my-6">
-              <label className="block text-lg mb-2 text-white" htmlFor="usernameField">Nome de Usuário</label>
-              <input className="w-full p-2 rounded-md text-lg bg-gray-800 bg-opacity-70 border border-gray-600 outline-blue-400 text-white" type="text" id="usernameField" value={username} onChange={(e) => setUsername(e.target.value)} />
+              <label className="block text-lg mb-2 text-white" htmlFor="usernameField"></label>
+             
             </div>
           )}
+          {/* Campo de email */}
           <div className="my-6">
-            <label className="block text-lg mb-2 text-white" htmlFor="emailField">Endereço de e-mail</label>
-            <input className="w-full p-2 rounded-md text-lg bg-gray-800 bg-opacity-70 border border-gray-600 outline-blue-400 text-white" type="email" id="emailField" value={email} onChange={(e) => setEmail(e.target.value)} />
+            <label className="block text-lg mb-2 text-white" htmlFor="emailField"> Email </label>
+            <input className="w-full p-2 rounded-md text-lg bg-gray-800 bg-opacity-70 border border-gray-600 outline-blue-400 text-white" type="email" id="emailField" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="email@example.com" />
           </div>
+          {/* Campo de senha */}
           <div className="my-6">
             <label className="block text-lg mb-2 text-white" htmlFor="passwordField">Senha</label>
-            <input className="w-full p-2 rounded-md text-lg bg-gray-800 bg-opacity-70 border border-gray-600 outline-blue-400 text-white" type="password" id="passwordField" value={password} onChange={(e) => setPassword(e.target.value)} />
+            <input className="w-full p-2 rounded-md text-lg bg-gray-800 bg-opacity-70 border border-gray-600 outline-blue-400 text-white" type="password" id="passwordField" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Insira sua senha" />
           </div>
-          {error && <p className="text-red-500">{error}</p>}
-          <div className="my-6">
+          {/* Checkbox para lembrar de mim */}
+          <div className="flex items-center my-4 text-white">
+            <input type="checkbox" id="rememberMe" checked={rememberMe} onChange={(e) => setRememberMe(e.target.checked)} className="mr-2" />
+            <label htmlFor="rememberMe">Mantenha conectado</label>
+          </div>
+          {/* Exibe mensagens de erro */}
+          {error && <p className="text-black-300">{error}</p>}
+          {/* Botão de envio */}
+          <div className="my-6 flex justify-between">
             <Button type="submit" className="w-full p-2 rounded-md text-lg font-bold border-none text-white bg-yellow-500">{isRegistering ? 'Cadastrar' : 'Entrar'}</Button>
+            {/* Link para alternar entre tela de registro e login */}
+            <div className="ml-4">
+              <a className="text-yellow-500 font-bold" href="#" onClick={() => setIsRegistering(true)}>Cadastre-se grátis</a>
+            </div>
           </div>
         </form>
+        {/* Link para recuperar senha ou alternar para tela de login */}
         <div className="text-center text-blue-900">
           {isRegistering ? (
             <>
@@ -75,7 +77,7 @@ const Login = () => {
             </>
           ) : (
             <>
-              Não tem uma conta? <a className="text-yellow-500 font-bold" href="#" onClick={() => setIsRegistering(true)}>Faça seu cadastro agora</a>
+              <a href="#">Recuperar senha</a>
             </>
           )}
         </div>
--- src/auth/Dashboard.tsx
+++
@@ -1,9 +0,0 @@
-export const Dashboard = () => {
-    return(
-        <div className="w-52">
-
-        </div>
-    );
-}
-
-export default Dashboard;
